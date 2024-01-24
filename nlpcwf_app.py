2f1573a2e1291d470b4ada2c67005a0c68a61bbe
kewchi <156171759+kewchi@users.noreply.github.com>
2024-01-24 17:27:13 +0800

    Update nlpcwf_app.py

--- nlpcwf_app.py
+++ nlpcwf_app.py
@@ -8,30 +8,43 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
 import streamlit as st
+import transformers
+from transformers import pipeline, DistilBertTokenizer, DistilBertForQuestionAnswering, AutoModelForQuestionAnswering, AutoTokenizer
 
-# Create the Streamlit app and set the title
-st.title("Streamlit Question Answering App 🦜 🦚")
+def load_model():
+    # Load DistilBERT model and tokenizer
+    tokenizer = transformers.AutoTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+    model = transformers.AutoModelForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad", return_dict=True).half()
 
-# Load the question answering model and tokenizer
-model_name = "deepset/roberta-base-squad2"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # Question answering pipeline
+    qa_pipe = transformers.pipeline("question-answering", model=model, tokenizer=tokenizer)
+    return qa_pipe
 
-# Create a question answering pipeline
-nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+qa_pipe = load_model()
 
-# Get user input
-question_input = st.text_input("Question:")
-context_input = st.text_area("Context:")
+# Streamlit app
+def main():
+    st.title("Red Panda Q&A")
+    
+    context = st.text_area("Context", value="The red panda (Ailurus fulgens), also known as the lesser panda, is a small mammal native to the eastern Himalayas and southwestern China. It has dense reddish-brown fur with a black belly and legs, white-lined ears, a mostly white muzzle and a ringed tail. Its head-to-body length is 51–63.5 cm (20.1–25.0 in) with a 28–48.5 cm (11.0–19.1 in) tail, and it weighs between 3.2 and 15 kg (7.1 and 33.1 lb). It is well adapted to climbing due to its flexible joints and curved semi-retractile claws. The red panda inhabits coniferous forests as well as temperate broadleaf and mixed forests, favouring steep slopes with dense bamboo cover close to water sources. It is solitary and largely arboreal. It feeds mainly on bamboo shoots and leaves, but also on fruits and blossoms. ")
+    # User input
+    question = st.text_area("Question", value=" ")
 
-# If the user has entered a question and context, process it
-if question_input and context_input:
-    QA_input = {
-        'question': question_input,
-        'context': context_input
-    }
-    res = nlp(QA_input)
-    st.text_area("Answer:", res['answer'])
-    st.write("Score:", res['score'])
+    # Answer button
+    if st.button("Get Answer"):
+      
+      # If input is not empty
+      if question != " ":
+        
+        # Get the answer using the pipeline
+        answer = qa_pipe(question=question, context=context)
+
+        # Display the answer
+        st.markdown(f"Answer: {answer['answer']}")
+        st.info(f"Confidence: {answer['score']}")
+
+if __name__ == "__main__":
+  main()
+
